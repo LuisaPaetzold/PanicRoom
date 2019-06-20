b933000e8fcf459215ea2647c9311f3516a20fa3
LuisaPaetzold <44864016+LuisaPaetzold@users.noreply.github.com>
2019-06-20 17:29:44 +0200

    Added first enemy/flashlight functionality

---
+++ PanicRoomProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject RetreatPosition;
+    public GameObject movePositions;
+    public float ScareTime = 2f;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+
+    public void ScareAway()
+    {
+        if (RetreatPosition != null)
+        {
+            transform.position = RetreatPosition.transform.position;
+        }
+    }
+}
---
+++ PanicRoomProject/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private float currentLightTimer = 0f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, 1, transform.forward, out hit, 100))
+        {
+            Enemy enemy = hit.collider.gameObject.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                currentLightTimer += Time.deltaTime;
+                if (currentLightTimer >= enemy.ScareTime)
+                {
+                    enemy.ScareAway();
+                    currentLightTimer = 0f;
+                }
+            }
+            else
+            {
+                currentLightTimer = 0f;
+            }
+        }
+        else
+        {
+            currentLightTimer = 0f;
+        }
+    }
+}
