caeab2016a70709b6edb662ce5099b549332ce33
LuisaPaetzold <44864016+LuisaPaetzold@users.noreply.github.com>
2019-07-09 11:57:44 +0200

    Win/Lose Sound effects

--- PanicRoomProject/Assets/Scripts/EndScreenUI.cs
+++ PanicRoomProject/Assets/Scripts/EndScreenUI.cs
@@ -8,22 +8,42 @@ public class EndScreenUI : MonoBehaviour
 {
     private GameResultSave save;
     private TextMeshProUGUI text;
+    private AudioSource audioSource;
+
+    public AudioClip WinClip;
+    public AudioClip LoseClip;
 
 
     void Start ()
     {
         text = GetComponentInChildren<TextMeshProUGUI>();
         save = FindObjectOfType<GameResultSave>();
+        audioSource = GetComponent<AudioSource>();
 
-        if (save != null && text != null)
+        if (save != null)
         {
-            if (save.gameWon)
+            if (text != null)
             {
-                text.text = "You survived!";
+                if (save.gameWon)
+                {
+                    text.text = "You survived!";
+                }
+                else
+                {
+                    text.text = "You are dead!";
+                }
             }
-            else
+
+            if (audioSource != null)
             {
-                text.text = "You are dead!";
+                if (save.gameWon)
+                {
+                    audioSource.PlayOneShot(WinClip);
+                }
+                else
+                {
+                    audioSource.PlayOneShot(LoseClip);
+                } 
             }
         }
     }
